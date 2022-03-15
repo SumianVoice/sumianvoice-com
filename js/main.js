122a16863f56bdb62b2f9f4b28213383345b71d5
Sumyjkl <sumianvoice@gmail.com>
2022-03-15 13:30:13 +1100

    Update CNAME, cv-style.css, and 18 more files...

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function copyEmail() {
+  /* Get the text field */
+  var copyText = document.getElementById("emaillinkdata");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  alert("Copied the email: " + copyText.value);
+}
