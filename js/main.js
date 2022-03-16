78fd80bb09c3b745bc615949017a45ce17a4324c
Sumyjkl <sumianvoice@gmail.com>
2022-03-16 18:22:00 +1100

    Update master.css, site-title.jpg, and 4 more files...

--- js/main.js
+++ js/main.js
@@ -12,3 +12,54 @@ function copyEmail() {
   /* Alert the copied text */
   alert("Copied the email: " + copyText.value);
 }
+
+// function checkCookie() {
+//   let hasVisited = getCookie("hasVisited");
+//   if (hasVisited == "true") {
+//     console.log("welcome back");
+//   }
+//   else {
+//     hasVisited = "true";
+//     setCookie("hasVisited", hasVisited, 365);
+//     document.getElementById("greeter").style.visibility = "visible";
+//   }
+// }
+
+function checkNew() {
+  let url = (window.location.href);
+  if (url.endsWith("#")) {
+    document.getElementById("greeter").style.visibility = "hidden";
+  }
+  else {
+    document.getElementById("greeter").style.visibility = "visible";
+    // window.location.replace('#');
+  }
+}
+
+// document.getElementById("greeter").style.visibility = "hidden";
+
+function closeGreeter() {
+  window.location.replace('#');
+  var greeter = document.getElementById("greeter");
+  // greeter.style.visibility = "hidden";
+  requestAnimationFrame(function(timestamp){
+      starttime = timestamp || new Date().getTime() //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date
+      greeterFade(timestamp, greeter, 1000, 500) // 400px over 1 second
+  })
+}
+
+var starttime
+function greeterFade(timestamp, el, dist, duration){
+    //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date:
+    var timestamp = timestamp || new Date().getTime()
+    var runtime = timestamp - starttime
+    var progress = runtime / duration
+    // progress = Math.min(progress, 1)
+    greeter.style.transform = "translateY(" + (-progress*100) + "vh)";
+    greeter.style.opacity = (1-progress);
+    if (runtime < duration){ // if duration not met yet
+        requestAnimationFrame(function(timestamp){ // call requestAnimationFrame again with parameters
+            greeterFade(timestamp, el, dist, duration)
+        })
+    }
+}
